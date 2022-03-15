224e86b5e1bbebf607f0b0d2487348eae4840600
heart <7362469@qq.com>
2022-03-15 20:43:07 +0800

    finish

--- src/pages/todoList/index.tsx
+++ src/pages/todoList/index.tsx
@@ -2,45 +2,65 @@ import React, { useReducer, useState } from 'react';
 import TodoUi from './todoUi';
 import { reducer, initReducer } from './reducer';
 import { rediEmum } from './reducer';
+import { Button, Input } from 'antd';
+const { TextArea } = Input;
 const Todo: React.FC = function () {
   const [num, setNum] = useState<number>(0);
+  const [context, setContext] = useState<string | undefined>(undefined);
   const [todoList, disPatchList] = useReducer(reducer, null, initReducer);
+  console.log('a');
+
   return (
-    <div>
+    <div className="flex">
       <TodoUi todoList={todoList} disPatch={disPatchList} />
       <div>
-        <button
-          onClick={() => {
-            disPatchList({
-              type: rediEmum.ADDLIST,
-              list: {
-                name: '新增的元素',
-                isFinish: false,
-              },
-            });
-          }}
-        >
-          添加list
-        </button>
-        <input
-          type="number"
-          value={num}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setNum(Number.parseInt((e.target as HTMLInputElement).value));
-          }}
-        />
-        <button
-          onClick={() => {
-            if (todoList && num >= 0 && num < todoList.length) {
+        <div>
+          <TextArea
+            placeholder="需要添加的todoList的内容"
+            value={context}
+            onChange={(e: React.ChangeEvent) => {
+              if (e.target instanceof HTMLTextAreaElement) {
+                setContext(e.target.value || '');
+              }
+            }}
+          />
+          <Button
+            onClick={() => {
               disPatchList({
-                type: rediEmum.REMOVELIST,
-                index: num,
+                type: rediEmum.ADDLIST,
+                list: {
+                  name: context ? context : '',
+                  isFinish: false,
+                },
               });
-            }
-          }}
-        >
-          删除list
-        </button>
+            }}
+          >
+            添加list
+          </Button>
+        </div>
+        <div>
+          <div>
+            <Input
+              type="number"
+              value={num}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setNum(Number.parseInt((e.target as HTMLInputElement).value));
+              }}
+            />
+          </div>
+          <Button
+            onClick={() => {
+              if (todoList && num >= 0 && num < todoList.length) {
+                disPatchList({
+                  type: rediEmum.REMOVELIST,
+                  index: num,
+                });
+              }
+            }}
+          >
+            删除list
+          </Button>
+        </div>
       </div>
     </div>
   );
