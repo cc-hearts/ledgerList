83b1675929e27f81ee6b95f036a550eed5bf6813
heart <7362469@qq.com>
2022-03-19 13:31:00 +0800

    page not found update layout

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import Less from './index.less';
+import { useHistory } from 'react-router';
 interface props {}
 const Error: React.FC<props> = function () {
+  const history = useHistory();
   return (
     <div className={Less['error']}>
-      <img src={require('../../assets/img/not_found.png')} alt="" />
       <div>
         <span>404</span>
         <span>Page not found</span>
+        <a
+          className="green-400"
+          onClick={() => {
+            history.go(-1);
+          }}
+        >
+          review more
+        </a>
       </div>
+      <img src={require('../../assets/img/not_found.png')} alt="" />
     </div>
   );
 };
