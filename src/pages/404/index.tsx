16ae99054ada9de2db34081cf8a13817d2028b35
heart <7362469@qq.com>
2022-03-19 12:04:44 +0800

    update page not found

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -4,8 +4,11 @@ interface props {}
 const Error: React.FC<props> = function () {
   return (
     <div className={Less['error']}>
-      <span></span>
-      <span></span>
+      <img src={require('../../assets/img/not_found.png')} alt="" />
+      <div>
+        <span>404</span>
+        <span>Page not found</span>
+      </div>
     </div>
   );
 };
